2a349e582d018812a09d0943419521cc82435135
poissonfish <niche@vt.edu>
2023-01-23 09:51:26 -0500

    update slide 1-2

---
+++ labs/lab_02/demo.sh
@@ -0,0 +1,38 @@
+# exectue scripts
+sh <your file>
+
+# copy files
+cp <your file> <destination>
+cp -r <your directory> <destination>
+
+# move/rename files
+mv <your file> <destination>
+mv <your file> <new name>
+
+# delete files
+rm <your file>
+rm -r <your directory>
+
+# create directory
+mkdir <your directory>
+
+# write to file
+echo "Hello World" > <your file>
+# append to file
+echo "Hello World" >> <your file>
+
+# print file contents
+cat <your file>
+
+# list files
+ls
+ls -alht
+
+# print working directory
+pwd
+
+# change directory
+cd <your directory>
+
+
+
