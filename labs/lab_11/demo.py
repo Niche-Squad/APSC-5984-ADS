a20feb45ade347f7839acb239cbb3ec972e3f244
poissonfish <niche@vt.edu>
2023-04-05 17:12:03 -0400

    lec demo

--- labs/lab_11/demo.py
+++ labs/lab_11/demo.py
@@ -25,4 +25,22 @@ plt.imshow(img_array)
 # so we put : in the third dimension
 plt.imshow(img_array[500:830, 1800:, :])
 
-#
+# lecture 11.2
+
+import numpy as np
+from scipy.signal import convolve2d
+import matplotlib.pyplot as plt
+
+img = np.zeros((10, 10))
+img[5:8, 5:8] = 1
+plt.imshow(img)
+
+# kernel 1
+kernel = np.array([[7, 1, 7], [1, -32, 1], [7, 1, 7]])
+output = convolve2d(img, kernel, mode="same")
+plt.imshow(output)
+
+# kernel 2
+kernel = np.array([[-7, -1, -7], [1, -2, 1], [7, 1, 7]])
+output = convolve2d(img, kernel, mode="same")
+plt.imshow(output)
